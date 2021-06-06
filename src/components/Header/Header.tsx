23525ab5201b44c2300519c94d486aa5eb0fd01b
Yuki Arami <wkgtyou0922@gmail.com>
2021-06-06 19:41:15 +0900

    ✨ CSS追加

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import { VFC } from "react";
 
 export const Header: VFC = () => {
-	return <div></div>;
+	return(
+    <div className="bg-white border-b-4 border-blue-100 min-h-full h-20 p-5">
+      <h1 className="line-height-100">divlog</h1>
+    </div>
+  ) 
 };
