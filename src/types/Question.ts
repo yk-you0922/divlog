8139310d35a20136744fb65fcb4d534d22791120
Yuki Arami <wkgtyou0922@gmail.com>
2021-06-13 22:33:31 +0900

    ♻️ リファクタリング

--- src/types/Question.ts
+++ src/types/Question.ts
@@ -2,7 +2,7 @@ export type Question = {
 	id?: number;
 	title: string;
 	description: string;
-	categoryId: string;
+	categoryId: number;
 	userId: number;
 	username: string;
 	created_at: string;
